99ec1f9880245b588450b2eb0348b81baba0134a
rayhan2392 <rkshawnso@gmail.com>
2025-05-21 02:21:22 +0600

    practice inner join and foreingn key

---
+++ foreignKey.sql
@@ -0,0 +1,42 @@
+CREATE Table "user" (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(25) NOT NULL
+)
+
+SELECT * FROM "user"
+
+CREATE Table post(
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES "user"(id)
+)
+
+--preventing posting from null id
+
+ALTER Table post
+ ALTER COLUMN user_id SET NOT NULL
+
+SELECT * FROM post
+
+INSERT INTO "user" (user_name) VALUES
+    ('John'),
+    ('Don'),
+    ('Harry'),
+    ('Brook')
+
+
+INSERT INTO post (title, user_id) VALUES
+('Learning PostgreSQL Basics', 1),
+('Building a REST API with Node.js', 2),
+('Frontend Tips for React Developers', 3),
+('Deploying MERN Stack Projects', 4);
+
+
+--try to insert a post with null id
+
+-- INSERT INTO post (title,user_id) VALUES('test',NULL)
+
+--Retrive all post title with thier user name
+
+SELECT title, user_name FROM post JOIN "user"
+ON post.user_id = "user".id
